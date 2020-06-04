cebab6f83297c3f8e58f38e2a9be17e4f578407a
Cagri <cagri.yildiz@tum.de>
2020-06-04 15:49:15 +0200

    first commit

---
+++ chexpert_downloader.py
@@ -0,0 +1,49 @@
+# GOOGLE DRIVE DOWNLOAD FUNCTIONS ARE TAKEN FROM THIS StackOverflow answer: https://stackoverflow.com/a/39225039
+import requests
+import os
+import pandas as pd
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def get_destination(name):
+    return ""
+    pass
+file_id_dict = {}
+data = pd.read_csv("./file_ids.csv",  header=None)
+for i in range(len(data)):
+    k = data[0][i]
+    v = data[1][i]
+    if type(v) != type("s"):
+        continue
+
+    parsed_id = v.split('file/d/')[1].split('/')[0]
+    file_id_dict[k] = parsed_id
+
+
+for k,v in file_id_dict.items():
+    destination = "./adjusted_data/%s"%str(k)
+    download_file_from_google_drive(v, destination)
