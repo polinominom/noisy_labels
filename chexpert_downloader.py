7ecbf98425af29ff9475207daee6a73d0c6c90c0
Cagri <cagri.yildiz@tum.de>
2020-06-07 16:58:11 +0200

    response save

--- chexpert_downloader.py
+++ chexpert_downloader.py
@@ -15,9 +15,10 @@ def download_file_from_google_drive(id, destination):
         save_response_content(response, destination)    
     else:
         print("Token confirmation failed!")
+        save_response_content(response, destination)
 
 def get_confirm_token(response):
-    print(response)
+    print(response.)
     for key, value in response.cookies.items():
         print("key: %s - value: %s"%(str(key),str(value)))
         if key.startswith('download_warning'):
