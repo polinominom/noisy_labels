4b21b4fc78d6b1457b87ba00c67ba1c28445851a
Cagri <cagri.yildiz@tum.de>
2020-10-15 13:25:28 +0200

    basic train changes

--- torch_basic/train.py
+++ torch_basic/train.py
@@ -2,6 +2,9 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+CUDA_VISIBLE_DEVICES = 0
+torch.cuda.set_device(torch.device('cuda:0'))
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
